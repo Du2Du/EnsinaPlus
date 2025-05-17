efee5a1a621ef73cc3e42f2310f3459e66d15a94
Du2Du <eduardohilario.eha@gmail.com>
2025-05-17 13:02:58 -0300

    fix: corrigindo sentry dsn

--- src/main/webui/src/main.ts
+++ src/main/webui/src/main.ts
@@ -4,7 +4,7 @@ import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
 Sentry.init({
-  dsn: 'https://@o4508059939962880.ingest.us.sentry.io/4508059941142528',
+  dsn: 'https://68c99e72e412fb211dadc7283d4b3231@o4508059939962880.ingest.us.sentry.io/4508059941142528',
   sendDefaultPii: true,
   integrations: [Sentry.browserTracingIntegration()],
   tracesSampleRate: 1.0,
